ca5ba08f4aca152b45a0d5835c3b76406d27c578
Enzo Ryan <20241pvai0030029@estudantes.ifpr.edu.br>
2026-08-22 10:37:32 -0300

    criando interface web

--- github.py
+++ github.py
@@ -1,16 +1,26 @@
 import os
 
 print("Configurando email")
-comando_email = "git config user.email \" 20241pvai0030029@estudantes.ifpr.edu.br \""
+
+comando_email = 'git config user.email "20241pvai0030029@estudantes.ifpr.edu.br"'
 os.system(comando_email)
+
 comando1 = "git add *"
 os.system(comando1)
-mensagem = input("Mensagem do commit ")
-while len(mensagem) < 5:
-    print(" Mensagem muito pequena, detalhe mais...")
-    mensagem = input("Mensagem do commit")
 
-comando2 = f"git commit -m \" {mensagem}\""
+mensagem = input("Mensagem do commit ")
+
+while len(mensagem) < 5:
+    print("Mensagem muito pequena, detalhe mais...")
+    mensagem = input("Mensagem do commit ")
+
+comando2 = f'git commit -m "{mensagem}"'
 os.system(comando2)
-comando3 = "git push"
+
+print("Atualizando o repositório...")
+comando3 = "git pull --rebase origin main"
 os.system(comando3)
+
+print("Enviando para o GitHub...")
+comando4 = "git push origin main"
+os.system(comando4)
