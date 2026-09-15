2f047acc43eaecd4e8e6492fb2e4961ca96b3238
unknown <20241pvai0030029@estudantes.ifpr.edu.br>
2026-09-15 09:38:34 -0300

    Inicio do peewee!!!!

---
+++ Peewee/orm.py
@@ -0,0 +1,14 @@
+from peewee import *
+Model
+SqliteDatabase
+
+
+#charfield - texto curto
+#integerfield - inteiros
+#floatfield - decimal
+#datetimefield - data e hora
+#booleanfield - booleano
+#foreignkeyfield - relação com outra tabela
+#default - valor padrão, null - vazio, unique - não pode repetir
+
+
